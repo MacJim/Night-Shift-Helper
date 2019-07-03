56b7ba34b03b83c685850d085de6098dc293af70
Jim Shi <shiyilin2008@gmail.com>
2019-07-03 19:30:27 +0800

    1. Adds functionality to automatically set the suitable color temperature based on the system time. 2. Adds functionality to enable or disable Night Shift.

--- Night Shift Helper/main.m
+++ Night Shift Helper/main.m
@@ -14,16 +14,64 @@
 #import "CBBlueLightClient.h"
 
 
+float getCurrentOptimalTemperature() {
+    NSDate* currentDate = [[NSDate alloc] init];
+    NSCalendar* currentCalendar = [NSCalendar currentCalendar];
+    NSDateComponents* dateComponents = [currentCalendar components:(NSCalendarUnitHour | NSCalendarUnitMinute) fromDate:currentDate];
+    NSInteger hour = [dateComponents hour];
+//    NSInteger minute = [dateComponents minute];
+    
+    switch (hour) {
+        //1. Warmest.
+        case 24:
+            //Fall through.
+        case 0 ... 5:
+            return 0.8;
+            
+        //2
+        case 23:
+            //Fall through.
+        case 6:
+            return 0.7;
+            
+        //3
+        case 19 ... 22:
+            //Fall through.
+        case 7:
+            return 0.5;
+            
+        //4
+        case 18:
+            //Fall through.
+        case 8:
+            return 0.3;
+            
+        //5. Coldest.
+        default:
+            return 0.1;
+            break;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         CBBlueLightClient* blueLightClient = [[CBBlueLightClient alloc] init];
         
         if (argc == 1) {
-            //TODO: Set night shift temperature automatically.
+            //Set Night Shift temperature automatically.
+            float optimalTemperature = getCurrentOptimalTemperature();
+            [blueLightClient setStrength:optimalTemperature commit:YES];
         } else if (argc == 2) {
-            //TODO: Enable or disable night shift.
+            //Enable or disable Night Shift.
+            if (strcmp(argv[1], "enable") == 0) {
+                [blueLightClient setEnabled:YES];
+            } else if (strcmp(argv[1], "disable") == 0) {
+                [blueLightClient setEnabled:NO];
+            } else {
+                printf("The second argument must be \"enable\" or \"disable\"!\n");
+            }
         } else if (argc == 3) {
-            //Set current night shift color temperature.
+            //Set current Night Shift color temperature.
             if (strcmp(argv[1], "set") != 0) {
                 printf("The second argument must be \"set\"!\n");
                 return 0;
